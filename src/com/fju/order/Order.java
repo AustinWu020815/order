85bdb723d66bc0f094ac576956df355545695f8e
AustinWu020815 <wu020707@gmail.com>
2022-04-29 16:10:41 +0800

    commit

--- src/com/fju/order/Order.java
+++ src/com/fju/order/Order.java
@@ -10,11 +10,19 @@ public class Order {
         this.delivery = delivery;
         shipFeed = delivery.price;
     }
+    /*
     public int total() {
         if (shipFeed == 19 && amount >= 199) {
             shipFeed = 0;
         }
         return amount + shipFeed;
-    }
+        public int total(){
+            if (delivery instanceof ShopeeDelivery){
 
+            }else {
+                return amount + shipFeed;
+            }
+        }
+    */
 }
+
